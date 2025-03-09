30d864c32b5dd862f56af4c5c14cc4d19fb4f7be
datawithanand <anandsuresh05@gmail.com>
2025-03-09 07:00:07 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -6,10 +6,8 @@ resource "aws_iam_role" "eks_role" {
     Statement = [
       {
         Effect    = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
+        Principal = { Service = "eks.amazonaws.com" }
+        Action    = "sts:AssumeRole"
       }
     ]
   })
@@ -18,4 +16,35 @@ resource "aws_iam_role" "eks_role" {
 resource "aws_iam_role_policy_attachment" "eks_policy" {
   role       = aws_iam_role.eks_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# IAM Role for Worker Nodes
+resource "aws_iam_role" "eks_nodes" {
+  name = "eks-worker-nodes-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "ec2.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "ec2_container_registry_readonly" {
+  role       = aws_iam_role.eks_nodes.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
