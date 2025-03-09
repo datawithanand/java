c8fb2d6a4b9a103cc046880763050e4e22bb0352
datawithanand <anandsuresh05@gmail.com>
2025-03-09 06:50:25 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+resource "aws_eks_cluster" "eks" {
+  name     = "my-k8s-cluster"
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = aws_subnet.eks_subnets[*].id
+  }
+}
+
+resource "aws_eks_node_group" "eks_nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "worker-nodes"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = aws_subnet.eks_subnets[*].id
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+  }
+}
